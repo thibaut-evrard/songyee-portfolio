28cf29e3b532ad1e9a57427238e5fe1025316a17
thibaut evrard <thibaut.evrard.1509@gmail.com>
2026-03-11 23:35:48 +0000

    Add basic carousel behaviour

--- frontend/src/components/Home/ProjectPreview/Carousel/index.tsx
+++ frontend/src/components/Home/ProjectPreview/Carousel/index.tsx
@@ -1,3 +1,4 @@
+import "./styles.scss";
 import { createSignal } from "solid-js";
 import RichMedia from "~/components/Shared/RichMedia";
 import { ProjectMediaContent } from "~/content/types";
@@ -7,12 +8,24 @@ interface Props {
 }
 
 export default function ProjectPreviewCarousel(props: Props) {
+  // oxlint-disable-next-line no-unassigned-vars
+  let containerRef: HTMLDivElement | undefined;
   const mediaLength = props.content.length;
   const [mediaIndex, setMediaIndex] = createSignal(0);
 
+  function updateMediaIndex() {
+    setMediaIndex((mediaIndex() + 1) % mediaLength);
+    if (containerRef) {
+      containerRef.scrollTo({
+        left: containerRef.clientWidth * mediaIndex(),
+        behavior: "smooth",
+      });
+    }
+  }
+
   return (
-    <button onclick={() => setMediaIndex((mediaIndex() + 1) % mediaLength)}>
-      <div>
+    <button onclick={updateMediaIndex} class="project-preview-carousel">
+      <div class="project-preview-carousel__content" ref={containerRef}>
         {props.content.map((media) => (
           <RichMedia content={media} />
         ))}
